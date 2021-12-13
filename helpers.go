1e9a03f4785d7f0d22bdeadf24929511fd1371fd
Eddie Barth <ebarth@medium.com>
2021-12-13 14:21:42 -0600

    Fixed an incorrect type reference & updated go.mod/sum

--- helpers.go
+++ helpers.go
@@ -1,69 +1,66 @@
 package main
 
 import (
-    "strings"
-    datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+	"strings"
 )
 
-
-// There are some WidgetDefinition types that don't even have a 
+// There are some WidgetDefinition types that don't even have a
 // Request type in their struct (or don't have Q in the Request struct). We want to ignore those, since
 // those won't contain a metric anyway.
 func CheckIgnoredWidgetDefs(object datadog.WidgetDefinition) bool {
 
-    switch {
-    case object.AlertGraphWidgetDefinition != nil:
-        return true
-    case object.AlertValueWidgetDefinition != nil:
-        return true
-    case object.ChangeWidgetDefinition != nil:
-        return true
-    case object.CheckStatusWidgetDefinition != nil:
-        return true
-    case object.EventStreamWidgetDefinition != nil:
-        return true
-    case object.EventTimelineWidgetDefinition != nil:
-        return true
-    case object.FreeTextWidgetDefinition != nil:
-        return true
-    case object.GroupWidgetDefinition != nil:
-        return true
-    case object.HostMapWidgetDefinition != nil:
-        return true
-    case object.IFrameWidgetDefinition != nil:
-        return true
-    case object.ImageWidgetDefinition != nil:
-        return true
-    case object.LogStreamWidgetDefinition != nil:
-        return true
-    case object.MonitorSummaryWidgetDefinition != nil:
-        return true
-    case object.NoteWidgetDefinition != nil:
-        return true
-    case object.SLOWidgetDefinition != nil:
-        return true
-    case object.ScatterPlotWidgetDefinition != nil:
-        return true
-    case object.ServiceMapWidgetDefinition != nil:
-        return true
-    case object.ServiceSummaryWidgetDefinition != nil:
-        return true
-    }
+	switch {
+	case object.AlertGraphWidgetDefinition != nil:
+		return true
+	case object.AlertValueWidgetDefinition != nil:
+		return true
+	case object.ChangeWidgetDefinition != nil:
+		return true
+	case object.CheckStatusWidgetDefinition != nil:
+		return true
+	case object.EventStreamWidgetDefinition != nil:
+		return true
+	case object.EventTimelineWidgetDefinition != nil:
+		return true
+	case object.FreeTextWidgetDefinition != nil:
+		return true
+	case object.GroupWidgetDefinition != nil:
+		return true
+	case object.HostMapWidgetDefinition != nil:
+		return true
+	case object.IFrameWidgetDefinition != nil:
+		return true
+	case object.ImageWidgetDefinition != nil:
+		return true
+	case object.LogStreamWidgetDefinition != nil:
+		return true
+	case object.MonitorSummaryWidgetDefinition != nil:
+		return true
+	case object.NoteWidgetDefinition != nil:
+		return true
+	case object.SLOWidgetDefinition != nil:
+		return true
+	case object.ScatterPlotWidgetDefinition != nil:
+		return true
+	case object.ServiceMapWidgetDefinition != nil:
+		return true
+	case object.ServiceSummaryWidgetDefinition != nil:
+		return true
+	}
 
-    return false
+	return false
 
 }
 
+func SearchExpressionsArray(expressionsArray *[]string, searchStr string) []string {
+	var resultArray []string
 
+	for _, expression := range *expressionsArray {
+		if strings.Contains(expression, searchStr) {
+			resultArray = append(resultArray, expression)
+		}
+	}
 
-func SearchExpressionsArray(expressionsArray *[]string, searchStr string) []string{
-    var resultArray     []string
-
-    for _, expression := range *expressionsArray {
-        if strings.Contains(expression, searchStr) {
-            resultArray = append(resultArray, expression)
-        }
-    }
-
-    return resultArray
+	return resultArray
 }
--- main.go
+++ main.go
@@ -10,121 +10,122 @@ import (
 )
 
 func main() {
-    var (
-        dashMap             = make(map[string]map[string]string)
-        metricSearchResults = make(map[string][]string)
-        monitorSearchResults = make(map[string][]string)
-        searchWaitGrp       = new(sync.WaitGroup)
+	var (
+		dashMap              = make(map[string]map[string]string)
+		metricSearchResults  = make(map[string][]string)
+		monitorSearchResults = make(map[string][]string)
+		searchWaitGrp        = new(sync.WaitGroup)
 
-        dashDetails         = flag.String("dash-details", "", "String: Get details for a specific dashboard by ID")
-        dashList            = flag.Bool("dash-list", false, "Bool: Will list all DataDog dashboards.")
-        metricFind          = flag.String("find-metric", "", "String: Search our DataDog dashboards for the provided metric name to see if it is in use")
-        monitorList         = flag.Bool("monitor-list", false, "Bool: Will list all DataDog monitors.")
-    )
+		dashDetails = flag.String("dash-details", "", "String: Get details for a specific dashboard by ID")
+		dashList    = flag.Bool("dash-list", false, "Bool: Will list all DataDog dashboards.")
+		metricFind  = flag.String("find-metric", "", "String: Search our DataDog dashboards for the provided metric name to see if it is in use")
+		monitorList = flag.Bool("monitor-list", false, "Bool: Will list all DataDog monitors.")
+	)
 
-    flag.Parse()
+	flag.Parse()
 
-    if *metricFind != "" {
-        searchWaitGrp.Add(1)
-        go func() {
-            fmt.Println("\nSearching for your metric across all dashboards.")
-            metricSearchResults = FindMetric(*metricFind)
-            fmt.Println("\n4. Feast your eyes upon the dashboards and monitors using your metric.")
+	if *metricFind != "" {
+		searchWaitGrp.Add(1)
+		go func() {
+			fmt.Println("\nSearching for your metric across all  & monitors.")
+			metricSearchResults = FindMetric(*metricFind)
+			fmt.Println("\n4. Feast your eyes upon the dashboards and monitors using your metric.")
 
-            jsonMetrics, err := json.MarshalIndent(metricSearchResults, "", "  ")
-            if err != nil {
-                fmt.Println("ERROR: \n  ", err)
-            }
+			jsonMetrics, err := json.MarshalIndent(metricSearchResults, "", "  ")
+			if err != nil {
+				fmt.Println("ERROR: \n  ", err)
+			}
 
-            fmt.Println(string(jsonMetrics))
+			fmt.Println(string(jsonMetrics))
 
-            var dashIds  []string
-            for dashId, _ := range metricSearchResults {
-                dashIds = append(dashIds, dashId)
-            }
+			var dashIds []string
+			for dashId, _ := range metricSearchResults {
+				dashIds = append(dashIds, dashId)
+			}
 
-            details := make(map[string]string)
-            for _, name := range dashIds {
-                details = GetDashboardDetails(name)
-                jsonDetails, _ := json.MarshalIndent(details, "", "  ")
-                fmt.Println(string(jsonDetails))
-            }
-            searchWaitGrp.Done()
-        }()
+			details := make(map[string]string)
+			for _, name := range dashIds {
+				details = GetDashboardDetails(name)
+				jsonDetails, _ := json.MarshalIndent(details, "", "  ")
+				fmt.Println(string(jsonDetails))
+			}
+			searchWaitGrp.Done()
+		}()
 
-        searchWaitGrp.Add(1)
-        go func() {
-            monitors := GetMonitors()
-            monitorsWaitGrp := new(sync.WaitGroup)
+		searchWaitGrp.Add(1)
+		go func() {
+			monitors := GetMonitors()
+			monitorsWaitGrp := new(sync.WaitGroup)
 
-            for _, monitor := range monitors {
-                monitorsWaitGrp.Add(1)
-                go func() {
-                    if strings.Contains(monitor.GetQuery(), *metricFind) {
-                        monitorSearchResults[monitor.GetName()] = append(monitorSearchResults[monitor.GetName()], monitor.GetQuery())
-                        }
+			for _, monitor := range monitors {
+				monitorsWaitGrp.Add(1)
+				go func() {
+					if strings.Contains(monitor.GetQuery(), *metricFind) {
+						monitorSearchResults[monitor.GetName()] = append(monitorSearchResults[monitor.GetName()], monitor.GetQuery())
+					}
 
-                    monitorsWaitGrp.Done()
-                    }()
+					monitorsWaitGrp.Done()
+				}()
 
-            monitorsWaitGrp.Wait()
-            }
+				monitorsWaitGrp.Wait()
+			}
 
-            searchWaitGrp.Done()
-        }()
+			searchWaitGrp.Done()
+		}()
 
-        searchWaitGrp.Wait()
+		searchWaitGrp.Wait()
 
-        jsonMonitors, err := json.MarshalIndent(monitorSearchResults, "", "  ")
-        if err != nil {
-            fmt.Println("ERROR: ", err)
-        }
+		jsonMonitors, err := json.MarshalIndent(monitorSearchResults, "", "  ")
+		if err != nil {
+			fmt.Println("ERROR: ", err)
+		}
 
-        fmt.Println(string(jsonMonitors))
+		fmt.Println(string(jsonMonitors))
 
-    } else if *dashList != false {
+	} else if *dashList != false {
 
-        dashMap = GetDashboards()
-        fmt.Println("Here is the list of dashboards in an attractive JSON format: ")
-        jsonDashboards, _ := json.MarshalIndent(dashMap, "", "  ")
-        fmt.Println(string(jsonDashboards))
+		dashMap = GetDashboards()
+		fmt.Println("Here is the list of dashboards in an attractive JSON format: ")
+		jsonDashboards, _ := json.MarshalIndent(dashMap, "", "  ")
+		fmt.Println(string(jsonDashboards))
 
-    } else if *dashDetails != "" {
+	} else if *dashDetails != "" {
 
-        details := GetDashboardDetails(*dashDetails)
-        jsonDetails, err := json.MarshalIndent(details, "", "  ")
-        if err != nil {
-            fmt.Println("ERROR: \n  ", err)
-        }
+		details := GetDashboardDetails(*dashDetails)
+		jsonDetails, err := json.MarshalIndent(details, "", "  ")
+		if err != nil {
+			fmt.Println("ERROR: \n  ", err)
+		}
 
-        fmt.Println("Here are the high-level details: ")
-        fmt.Println(string(jsonDetails))
+		fmt.Println("Here are the high-level details: ")
+		fmt.Println(string(jsonDetails))
 
-        // fmt.Println("Widget ID's and expressions are: ")
-        // fmt.Println(string(expressionJson))
-    } else if *monitorList != false {
-        var (
-            monitorMap = make(map[string]map[string]string)
-            monitorId  string
-        )
-        monitors := GetMonitors()
+		// fmt.Println("Widget ID's and expressions are: ")
+		// fmt.Println(string(expressionJson))
+	} else if *monitorList != false {
+		var (
+			monitorMap = make(map[string]map[string]string)
+			monitorId  string
+		)
+		monitors := GetMonitors()
 
-        for _, monitor := range monitors {
-            monitorId = strconv.FormatInt(monitor.GetId(), 10)
-            monitorMap[monitor.GetName()] = map[string]string{
-                "ID": monitorId,
-                "Creator": *monitor.Creator.Name,
-                "Query": monitor.GetQuery(),
-            }
-        }
+		for _, monitor := range monitors {
+			monitorId = strconv.FormatInt(monitor.GetId(), 10)
+			monitorCreatorName := monitor.Creator.GetName()
+			monitorMap[monitor.GetName()] = map[string]string{
+				"ID":      monitorId,
+				"Creator": monitorCreatorName,
+				"Query":   monitor.GetQuery(),
+			}
+		}
 
-        jsonMonitors, err := json.MarshalIndent(monitorMap, "", "  ")
-        if err != nil {
-            fmt.Printf("ERROR: %v\n", err)
-        }
+		jsonMonitors, err := json.MarshalIndent(monitorMap, "", "  ")
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
 
-        fmt.Println("Here are all of the monitors & some info: \n")
-        fmt.Println(string(jsonMonitors))
-    }
+		fmt.Println("Here are all of the monitors & some info: \n")
+		fmt.Println(string(jsonMonitors))
+	}
 
 }
